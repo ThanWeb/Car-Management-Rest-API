5aaec88c94eb155780a1d1acb06428290273b0dc
Hans <classsickpeople@gmail.com>
2023-04-28 22:28:14 +0800

    Konfigurasi cors

--- app/index.js
+++ app/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
-
+const cors = require('cors')
 const app = express()
+const corsOption = {
+    origin: 'http://localhost:3000'
+}
 
 const indexRouter = require('../config/routes/index')
 const usersRouter = require('../config/routes/users')
@@ -10,6 +13,7 @@ const carsRouter = require('../config/routes/cars')
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }))
+app.use(cors(corsOption))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
