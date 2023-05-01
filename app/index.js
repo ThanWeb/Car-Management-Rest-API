ee37b321eda03220e5304de171dac1f191ee2da1
Hans <classsickpeople@gmail.com>
2023-05-01 11:49:26 +0800

    Use dotenv and cookie-parser

--- app/index.js
+++ app/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+
+dotenv.config()
 const app = express()
 const corsOption = {
     origin: 'http://localhost:3000'
@@ -14,6 +18,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }))
 app.use(cors(corsOption))
+app.use(cookieParser())
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
