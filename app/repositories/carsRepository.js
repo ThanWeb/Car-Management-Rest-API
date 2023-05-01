fb38b56f688c69e1a507050bdd9cecabd695110f
Hans <classsickpeople@gmail.com>
2023-05-02 00:26:35 +0800

    Update query for car list

--- app/repositories/carsRepository.js
+++ app/repositories/carsRepository.js
@@ -2,7 +2,15 @@ const { Cars } = require('../models')
 
 module.exports = {
     findAll () {
-        return Cars.findAll()
+        return Cars.findAll({
+            attributes: ['name', 'type', 'size', 'rentPerDay'],
+            where: {
+                isDeleted: false
+            },
+            order: [
+                ['name', 'ASC']
+            ]
+        })
     },
     getTotalCars () {
         return Cars.count()
--- config/routes/cars.js
+++ config/routes/cars.js
@@ -5,33 +5,9 @@ const router = express.Router()
 const controllers = require('../../app/controllers')
 
 router.get('/', controllers.api.cars.list)
-
-router.get('/:id', (req, res, next) => {
-    res.json({
-        status: 200,
-        message: 'Car Detail'
-    })
-})
-
-router.post('/add', (req, res, next) => {
-    res.json({
-        status: 201,
-        message: 'Add Car'
-    })
-})
-
-router.post('/edit/:id', (req, res, next) => {
-    res.json({
-        status: 200,
-        message: 'Edit Car'
-    })
-})
-
-router.post('/delete/:id', (req, res, next) => {
-    res.json({
-        status: 200,
-        message: 'Delete Car'
-    })
-})
+// router.post('/add', verifyToken, controllers.api.cars.add)
+// router.put('/edit', verifyToken, controllers.api.cars.edit)
+// router.get('/detail/:id', verifyToken, controllers.api.cars.detail)
+// router.delete('/delete', verifyToken, controllers.api.cars.delete)
 
 module.exports = router
